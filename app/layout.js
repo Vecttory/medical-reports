dd313c8aae2d963d813d237449dda3a01a51bcf7
Alberto <alberto.ola.dev@gmail.com>
2026-03-06 09:22:52 -0600

    verce analitics added to app

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "../components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       >
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
